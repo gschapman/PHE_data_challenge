c671d9b03ea04375014387f3b4fc4cf0bda28cf9
Greg Chapman <gchapman@neoninc.org>
2022-11-30 13:09:52 -0500

    Create table per species, per date, with "yes" status tallies. Current method inefficient

--- PHE_data_challenge.R
+++ PHE_data_challenge.R
@@ -9,7 +9,7 @@ library(neonUtilities)
 
 ### Inputs
 
-siteid <- "SCBI"
+siteid <- "SERC"
 clear.cache <- F # TRUE to re-download PHE data for sites already loaded
 
 
@@ -28,4 +28,37 @@ if(!exists("phe.cache") | clear.cache) phe.cache <- data.frame()
 if(!siteid %in% phe.cache$siteID)
   phe.cache <- rbind(getPortal_PHE_status(siteid), phe.cache)
 
+
+## All phe obs data
 phe <- phe.cache[phe.cache$siteID==siteid,]
+
+## Create table per species, per date
+phe.spp <- unique(phe[,c("date", "taxonID", "scientificName", "taxonRank", "growthForm")])
+
+
+phephases <- unique(phe$phenophaseName)
+
+# Initialize columns per extant phenophase
+for(phephase in phephases)
+  phe.spp[,phephase] <- NA
+
+
+## Tally 'yes' status per species, per date
+
+# Develop 'apply' method if time; this method is time consuming
+writeLines(c("", "Tallying per-date status. Processing may take up to 30 seconds..."))
+
+for(i in 1:nrow(phe.spp)){
+  
+  phe.i <- phe[phe$taxonID == phe.spp$taxonID[i]
+               & phe$date == phe.spp$date[i],]
+  
+  for(phephase in phephases){
+    
+    phe.spp[,phephase][i] <- nrow(
+      phe.i[phe.i$phenophaseName == phephase
+            & phe.i$phenophaseStatus == "yes",]
+    )
+  }
+  
+}
