a440be8adb24cd8f77b83268a111473fd91f8082
Greg Chapman <gchapman@neoninc.org>
2022-11-30 12:18:21 -0500

    basic portal data load/merge

---
+++ PHE_data_challenge.R
@@ -0,0 +1,54 @@
+
+rm(list=ls()) # Clear variables
+graphics.off() # Close figures
+
+
+library(neonUtilities)
+
+
+### Inputs
+
+siteid <- "SCBI"
+
+
+## Get PHE portal data
+
+phe <- loadByProduct(
+  dpID = "DP1.10055.001",
+  site = siteid,
+  startdate = "2017-01", # Only data post-Fulcrum
+  enddate = "2022-11",
+  check.size = F
+)
+
+## Process phe_perindividual
+phe.ind <- phe[["phe_perindividual"]]
+
+# Keep most recent per dpug if dups exist
+# Sort by ID and editedDate, remove duplicates (leaving last value i.e. most recent version)
+# any(is.na(phe.ind$editedDate))
+phe.ind <- phe.ind[with(phe.ind, order(individualID, editedDate)),]
+phe.ind <- phe.ind[!duplicated(phe.ind$individualID, fromLast = T),]
+
+
+
+## Processs phe_statusintensity
+phe.stat <- phe[["phe_statusintensity"]]
+
+# Keep most recent per dpug if dups exist
+# Sort by ID, phenophaseName, date, and editedDate, remove duplicates
+# any(is.na(phe.stat$editedDate))
+phe.stat <- phe.stat[with(phe.stat, order(individualID, phenophaseName, date, editedDate)),]
+phe.stat <- phe.stat[!duplicated(phe.stat[,c("individualID", "phenophaseName", "date")], fromLast = T),]
+
+
+
+## Merge relevant perindividual data
+phe.stat.ind <- merge(
+  phe.stat,
+  phe.ind[,c("individualID", "transectMeter", "directionFromTransect", "ninetyDegreeDistance",
+             "taxonID", "scientificName", "identificationQualifier", "taxonRank", "growthForm")],
+  all.x = T
+)
+
+# any(is.na(phe.stat.ind$growthForm))
